69380ea84b723ccfeb0548681eafb2321f05961c
mrarmyant <taylor.fry@fryco.com>
2021-06-24 19:36:55 -0500

    can add and delete individual photos, now working on updating

--- controllers/log.controller.js
+++ controllers/log.controller.js
@@ -1,9 +1,8 @@
 const Boat = require('../models/boat.model');
 const router = require('express').Router();
 
-router.route('/new/:id').post((req, res)=>{
+router.route('/:id').post((req, res)=>{
     console.log("Attempting to add Log to Boat")
-    console.log(req.body)
     Boat.updateOne({
                   _id: req.params.id
                 }, {
@@ -12,6 +11,67 @@ router.route('/new/:id').post((req, res)=>{
                   }
                 }).then(boat => res.json(boat))
                 .catch(err => res.status(400).json('Error! ' + err))
+});
+
+router.route('/:id').delete((req, res)=>{
+  console.log("Attempting to remove a Log from Boat")
+  Boat.updateOne(
+    {_id: req.params.id},
+    { $pull: {logs : { _id: req.body.logID} }}, {new: true}
+              ).then(boat => res.json(boat))
+              .catch(err => res.status(400).json('Error! ' + err))
+});
+
+// add something here to remove from wasabi bucket
+// router.route('/photos/:id').delete((req, res)=>{
+//   console.log("Attempting to remove a Log from Boat")
+//   console.log(req.body)
+//   Boat.updateOne(
+//     {_id: req.params.id},
+//     { $pull: { logs : { _id: req.body.photoID} }}, {new: true}
+//               ).then(boat => res.json(boat))
+//               .catch(err => res.status(400).json('Error! ' + err))
+// });
+
+router.route('/photos/:id').delete((req, res)=>{
+  console.log("Attempting to remove a photo from a Log from a Boat")
+  console.log(req.body)
+  Boat.updateOne(
+        {_id: req.params.id},
+        {$pull: {
+          "logs.$[].photos":{_id : req.body.photoID} }},
+        { new: true }
+      )
+      .then(boat => res.json(boat))
+      .catch(err => res.status(400).json('Error! ' + err))
+});
+
+router.route('/photos/:id').post((req, res)=>{
+  console.log("Attempting to add photo to a Log to a Boat")
+  console.log(req.body)
+  Boat.updateOne({
+                _id: req.params.id,
+                "logs._id": req.body.logID
+              }, {
+                $addToSet: {
+                  "logs.$[].photos": req.body.photo
+                }
+              }).then(boat => res.json(boat))
+              .catch(err => res.status(400).json('Error! ' + err))
+});
+
+router.route('/photos/:id').put((req, res)=>{
+  console.log("Attempting to add photo to a Log to a Boat")
+  console.log(req.body)
+  Boat.updateOne({
+                _id: req.params.id,
+                "logs._id": req.body.logID
+              }, {
+                $addToSet: {
+                  "logs.$[].photos": req.body.photo
+                }
+              }).then(boat => res.json(boat))
+              .catch(err => res.status(400).json('Error! ' + err))
 });
 
 // router.route('/overhaul/:id').put((req, res)=>{
