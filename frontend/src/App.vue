7fbf426876cb134d1f9450c21022b5b2715db19c
mrarmyant <taylor.fry@fryco.com>
2021-06-25 20:29:26 -0500

    adding form generator

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <Boat msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import Boat from "./components/Boat.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    Boat,
   },
 };
 </script>
---
+++ frontend/src/components/Boat.vue
@@ -0,0 +1,49 @@
+<template>
+  <SchemaForm :schema="mySchema" />
+</template>
+
+<script>
+import { ref } from "vue";
+import { SchemaForm, useSchemaForm } from "formvuelate";
+
+export default {
+  components: { SchemaForm },
+  setup() {
+    const formModel = ref({});
+    useSchemaForm(formModel);
+
+    const mySchema = ref({
+      name: {
+        component: FormText,
+      },
+      photoLoc: {},
+      make: {
+        component: FormText,
+      },
+      model: {
+        component: FormText,
+      },
+      length: {
+        component: FormText,
+      },
+      serial: {
+        component: FormText,
+      },
+      year: {
+        component: FormText,
+      },
+    });
+
+    return {
+      mySchema,
+    };
+  },
+};
+</script>
+
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
